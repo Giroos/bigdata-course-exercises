806cfdd8e5ae7cb8126d6a4b8d81eb2d97638537
Giroos <jgenik@gmail.com>
2020-07-26 11:35:12 +0300

    word count web application

---
+++ src/main/java/com/gena/exercises/topxwords/TopXWordsApplication.java
@@ -0,0 +1,12 @@
+package com.gena.exercises.topxwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopXWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopXWordsApplication.class);
+    }
+}
